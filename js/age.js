a305bde22bcc3a877053bd6124834319a4a7ca3c
obada <obadatumah19@gmail.com>
2020-02-04 19:41:49 +0200

    change to the if statement

--- js/age.js
+++ js/age.js
@@ -1,14 +1,20 @@
 var date = new Date();
+
 var m = date.getMonth();
 var y = date.getFullYear();
 var name = prompt("what is your name?");
-var year = prompt("what is your year?");
+var year = prompt("what year were you born?");
 if (year > 1940 && year <=2020) {
     var age1= y-year;
-    var month = prompt("what is your month?");
+    var month = prompt("what month were you born?");
     if(month >1 && month <=12 ){
+        
         var age2= m-month;
         Math.abs(age2);
+        if (month >m) {
+            age1=age1-1;
+        }
+        else{}
         document.write('<h3>'+"OK"+name+" your age is"+age1+"years, And"+age2+"months"+'</h3>')
 
     }
@@ -20,6 +26,7 @@ if (year > 1940 && year <=2020) {
 } else {
     document.write('<h3>'+"This year is not valid!"+'</h3>')
 }
+
 
 
 
