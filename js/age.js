6e16d812add96877d1ad58da19d7009a460efca3
obada <obadatumah19@gmail.com>
2020-02-05 11:31:35 +0200

    adding function

--- js/age.js
+++ js/age.js
@@ -1,25 +1,18 @@
-var date = new Date();
 
-var m = date.getMonth()+1;
-var y = date.getFullYear();
+function calAge(a,b){
 var fname = prompt("what is your first name?");
 var lname = prompt("what is your last name?");
 var year = prompt("what year were you born?");
 if (year > 1940 && year <=2020) {
-    var age1= y-year;
+    var age1= a-year;
     var month = prompt("what month were you born?");
     if(month >=1 && month <=12 ){
-        
-        
 
-        
-        
-       
-        if (month >m) {
+        if (month >b) {
             age1=age1-1;
         }
         
-        document.write('<h3>'+"OK "+fname+" "+lname+"  your age is  "+age1+" years"+'</h3>');
+        return ('<h3>'+"OK "+fname+" "+lname+"  your age is  "+age1+" years"+'</h3>');
 
     }
     else{
@@ -29,7 +22,13 @@ if (year > 1940 && year <=2020) {
     
 } else {
     document.write('<h3>'+"This year is not valid!"+'</h3>')
-}
+}}
+var date = new Date();
+
+var m = date.getMonth()+1;
+var y = date.getFullYear();
+var final = calAge(y,m);
+document.write(final);
 
 
 
