2856c7feb899c4012d2a7af0c389ffc39e1f0a96
obada <obadatumah19@gmail.com>
2020-02-05 16:35:21 +0200

    change in js file

--- js/age.js
+++ js/age.js
@@ -3,6 +3,7 @@ function calAge(a,b){
 var fname = prompt("what is your first name?");
 var lname = prompt("what is your last name?");
 var year = prompt("what year were you born?");
+
 if (year > 1940 && year <=2020) {
     var age1= a-year;
     var month = prompt("what month were you born?");
@@ -25,12 +26,50 @@ if (year > 1940 && year <=2020) {
      document.write('<h3>'+"This year is not valid!"+'</h3>')
     
 }}
+
+
+
+
+
+function loops(){
+    var star = prompt("Choose an operator to see the loop (*/+)");
+    
+    while (star !== '*' && star !=='+') {
+         star = prompt("Choose an operator to see the loop (*/+)");
+    }
+    var num = prompt("How many lines do you want?");
+    if(star === '*'){
+    for (var i=0 ; i<num;i++){
+        document.write('<h3>'+"***************************"+'</h3>')
+        
+    }
+    var image = '<img src="starss.png">';
+    document.write('<h3>'+image+'</h3>');
+    }
+    else if(star === '+'){
+    for (var i=0 ; i<num;i++){
+        document.write('<h3>'+"+++++++++++++++++++++++++++++"+'</h3>')
+        
+    }
+    var image = '<img src="pluss.jpg">';
+    document.write('<h3>'+image+'</h3>');
+    }
+    else{
+        document.write('<h3>'+"The OPERATOR is not valid!"+'</h3>')
+    }
+}
+
+
 var date = new Date();
 
 var m = date.getMonth()+1;
 var y = date.getFullYear();
 var final = calAge(y,m);
 document.write(final);
+loops();
+
+
+//var s = '<img src="anythin.png">';
 
 
 
